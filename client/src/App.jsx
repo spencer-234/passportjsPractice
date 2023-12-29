1b4322cfe13740ab5e00e276cd570df178043e04
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-12-29 05:24:14 -0500

    implement react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,66 @@ import Home from "./pages/Home/Home.jsx";
 import Posts from "./pages/Posts/Posts.jsx";
 import Login from "./pages/Login/Login.jsx";
 import "./app.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 
 function App() {
 
+  const user = false;
+
+  const ProtectedRoute = ({ children }) => {
+    if (!user) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+    )
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/posts/:id",
+          element: <Posts />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: (
+        <>
+          <Navbar />
+          <Login />
+        </>
+      ),
+    },
+  ]);
+
   return (
-    <>
-      <Navbar />
-      <Login />
-    </>
+    <RouterProvider router={router} />
   )
 }
 
