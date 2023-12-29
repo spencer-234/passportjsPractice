cc3d858906ba9fe8231edf49eb095fecf95aab4e
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-12-28 20:51:08 -0500

    set up login page and styling

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,10 +1,37 @@
 import "./login.scss";
+import Google from "../../assets/social_icons/google.png";
+import Github from "../../assets/social_icons/github.png";
+import Facebook from "../../assets/social_icons/facebook.png";
 
 export default function Login() {
   return (
-    <div>
-      <h1 className="loginTitle">Choose a login method</h1>
-      <div className="wrapper"></div>
+    <div className="login">
+      <h1 className="loginTitle">Choose a Login Method</h1>
+      <div className="wrapper">
+        <div className="left">
+          <div className="loginButton google">
+            <img src={Google} alt="" />
+            Google
+          </div>
+          <div className="loginButton facebook">
+            <img src={Facebook} alt="" />
+            Facebook
+          </div>
+          <div className="loginButton github">
+            <img src={Github} alt="" />
+            Github
+          </div>
+        </div>
+        <div className="center">
+          <div className="line"></div>
+          <div className="or">OR</div>
+        </div>
+        <div className="right">
+          <input type="text" placeholder="Username" />
+          <input type="text" placeholder="Password" />
+          <button className="submit">Login</button>
+        </div>
+      </div>
     </div>
   )
 }
